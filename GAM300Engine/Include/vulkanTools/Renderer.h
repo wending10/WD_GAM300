66c1e7ba740d05e0d360b402d6ea66cd60f6f150
AmadeusNg <90673660+AmadeusNg@users.noreply.github.com>
2023-10-01 23:41:22 +0800

    finished commenting (#53)
    * updated to fragment lighting
    
    * commenting of functions
    
    * Delete GAM300Editor/imgui.ini
    
    * update with the correct dll and ini
    
    * redid commenting of functions
    
    * finished commenting
    
    * Update GAM300Engine.dll
    
    ---------
    
    Co-authored-by: Taka <90673673+wending10@users.noreply.github.com>

--- GAM300Engine/Include/vulkanTools/Renderer.h
+++ GAM300Engine/Include/vulkanTools/Renderer.h
@@ -1,3 +1,14 @@
+/*!*************************************************************************
+****
+\file Renderer.h
+\author Ng Zuo Xian Amadeus
+\par DP email: ng.z@digipen.edu
+\par Course: CSD3400
+\par Section: a
+\date 22-9-2023
+\brief  Contains the Renderer Class and its member functions
+****************************************************************************
+***/
 #ifndef TDS_VULKAN_RENDERER
 #define TDS_VULKAN_RENDERER
 
@@ -8,33 +19,38 @@
 namespace TDS {
 	class DLL_API Renderer {
 	public:
+		//Constructor and Destructor
 		Renderer(WindowsWin& window, VulkanInstance& instance);
 		~Renderer();
 
+		//no copy constructor
 		Renderer(const Renderer&) = delete;
 		Renderer& operator=(const Renderer&) = delete;
 
-		VkRenderPass getSwapChainRenderPass() const { return m_SwapChain->getRenderPass(); }
-		float getAspectRatio() const { return m_SwapChain->extentAspectRatio(); }
-		bool isFrameInProgress() const { return m_isFrameStarted; }
-		VkImageView getImageView(uint32_t val)const { return m_SwapChain->getImageView(val); }
-		std::vector	<VkImageView> getImageViewContainer()const { return m_SwapChain->getImageViewContainer(); }
-		VkCommandBuffer getCurrentCommandBuffer() const {
+		//Getters
+		VkRenderPass				getSwapChainRenderPass() const { return m_SwapChain->getRenderPass(); }
+		float						getAspectRatio() const { return m_SwapChain->extentAspectRatio(); }
+		bool						isFrameInProgress() const { return m_isFrameStarted; }
+		VkImageView					getImageView(uint32_t val)const { return m_SwapChain->getImageView(val); }
+		std::vector	<VkImageView>	getImageViewContainer()const { return m_SwapChain->getImageViewContainer(); }
+		VkCommandBuffer				getCurrentCommandBuffer() const {
 			assert(m_isFrameStarted && "Cannot get commandbuffer when frame not in progress");
 			return m_vCommandBuffers[m_currentFrameIndex];
 		}
 
-		int getFrameIndex() const {
+		int							getFrameIndex() const {
 			assert(m_isFrameStarted && "Cannot get Frame index when frame not in progress");
 			return m_currentFrameIndex;
 		}
 
+		//these functions tell tell when to begin and end the frame and renderpass
 		VkCommandBuffer BeginFrame();
 		void EndFrame();
 		void BeginSwapChainRenderPass(VkCommandBuffer commandbuffer);
 		void EndSwapChainRenderPass(VkCommandBuffer commandbuffer);
 
 	private:
+		//helper functions
 		void createCommandBuffers();
 		void freeCommandBuffers();
 		void recreateSwapChain();
