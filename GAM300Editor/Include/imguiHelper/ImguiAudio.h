a6aa0883e2a53751e9510deeb75a78e705a9321b
WongCSheng <90675917+WongCSheng@users.noreply.github.com>
2023-10-01 20:39:22 +0800

    Audio and gitignore (#49)
    * updated dll
    
    newer version of fmod dll and lib
    
    * DLL fix
    
    Copy non-logger fmod dll to solution directory
    
    * Wipe
    
    * Replace from ported-engine
    
    * Changed opening studio
    
    Changed from system() to shellexecute()
    
    * Update .gitignore
    
    * Added back dll
    
    * Changed gitignore
    
    commented *.exe in gitignore
    
    * Update imgui.ini
    
    * fixes-wd
    
    ---------
    
    Co-authored-by: wending10 <90673673+wending10@users.noreply.github.com>

--- GAM300Editor/Include/imguiHelper/ImguiAudio.h
+++ GAM300Editor/Include/imguiHelper/ImguiAudio.h
@@ -1,3 +1,14 @@
+/*!*************************************************************************
+****
+\file ImguiAudio.h
+\author w.chongsheng
+\par DP email: w.chongsheng@digipen.edu
+\par Course: CSD3400
+\par Section: a
+\brief  Imgui UI in charge of audio manipulations
+****************************************************************************
+***/
+
 #ifndef IMGUI_AUDIO
 #define IMGUI_AUDIO
 
@@ -9,13 +20,35 @@ namespace TDS
 	class AudioImgui : public LevelEditorPanel
 	{
 	public:
+		/**
+		 * @brief Constructor that sets appear to false and bar as 0.f. Initiates the Audio Engine.
+		*/
 		AudioImgui();
+		/**
+		 * @brief Destructor that deactivates the Audio Engine
+		*/
 		~AudioImgui();
 
+		/**
+		 * @brief Shows ImGui Controls for audio when user navigates to audio assets in the editor.
+		*/
+		void activate_audio_controls();
+
+		/**
+		 * @brief Hides ImGui Constrols for audio.
+		*/
+		void deactivate_audio_controls();
+
+		/**
+		 * @brief Update loop.
+		*/
 		void update();
 
 	private:
-		AudioEngine audeng;
+		AudioWerks::AudioEngine audeng;
+
+		bool appear;
+		float bar;
 	};
 }
 
