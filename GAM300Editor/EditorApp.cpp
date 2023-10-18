e046082e92105e5b2ecb94d394e5a83c28eb64e1
RunicBoi <eltonator1@hotmail.com>
2023-10-18 22:23:06 +0800

    Added Debug tools to scripting, added script active toggling and entity toggling

--- GAM300Editor/EditorApp.cpp
+++ GAM300Editor/EditorApp.cpp
@@ -119,21 +119,43 @@ namespace TDS
     {
         ShaderReflector::GetInstance()->Init("../assets/shaders/", "../assets/shaders/AllShaders.bin");
         GraphicsManager::getInstance().Init(&m_window);
-
         m_AssetManager.Init();
         m_AssetManager.PreloadAssets();
         SceneManager::GetInstance()->Init();
-
         ecs.initializeSystems(1);
         Run();
     }
 
     void Application::Update()
     {
+        auto executeUpdate = GetFunctionPtr<void(*)(void)>
+            (
+                "ScriptAPI",
+                "ScriptAPI.EngineInterface",
+                "ExecuteUpdate"
+            );
+
+        auto reloadScripts = GetFunctionPtr<void(*)(void)>
+            (
+                "ScriptAPI",
+                "ScriptAPI.EngineInterface",
+                "Reload"
+            );
+
+        auto addScript = GetFunctionPtr<bool(*)(int, const char*)>
+            (
+                "ScriptAPI",
+                "ScriptAPI.EngineInterface",
+                "AddScriptViaName"
+            );
+        auto toggleScript = GetFunctionPtr<bool(*)(int, const char*)>
+            (
+                "ScriptAPI",
+                "ScriptAPI.EngineInterface",
+                "ToggleScriptViaName"
+            );
 
         RendererSystem::assetManager = &m_AssetManager;
-
-
         initImgui();
         float lightx = 0.f;
         GraphicsManager::getInstance().setCamera(m_camera);
@@ -154,46 +176,35 @@ namespace TDS
             imguiHelper::Update();
 
             GraphicsManager::getInstance().GetSwapchainRenderer().BeginSwapChainRenderPass(commandBuffer);
+
             if (isPlaying)
             {
                 ecs.runSystems(1, DeltaTime);
             }
-            auto executeUpdate = GetFunctionPtr<void(*)(void)>
-                (
-                    "ScriptAPI",
-                    "ScriptAPI.EngineInterface",
-                    "ExecuteUpdate"
-                );
-
-            auto reloadScripts = GetFunctionPtr<void(*)(void)>
-                (
-                    "ScriptAPI",
-                    "ScriptAPI.EngineInterface",
-                    "Reload"
-                );
-
-            auto addScript = GetFunctionPtr<bool(*)(int, const char*)>
-                (
-                    "ScriptAPI",
-                    "ScriptAPI.EngineInterface",
-                    "AddScriptViaName"
-                );
 
             imguiHelper::Draw(commandBuffer);
             GraphicsManager::getInstance().GetSwapchainRenderer().EndSwapChainRenderPass(commandBuffer);
             GraphicsManager::getInstance().EndFrame();
-            Input::scrollStop();
-            //Submit the swapchain rendering for the current frame
+            if (GetKeyState(VK_F5) & 0x8000)
+            {
+                compileScriptAssembly();
+                reloadScripts();
+                addScript(1, "Test");
+            }
 
-            //executeUpdate();
+            if (GetKeyState(VK_SPACE) & 0x8000)
+            {
+                toggleScript(1, "Test");
+            }
+            executeUpdate();
+            Input::scrollStop();
         }
-        //stopScriptEngine();
+        stopScriptEngine();
         /*vkDeviceWaitIdle(m_pVKInst.get()->getVkLogicalDevice());*/
         vkDeviceWaitIdle(GraphicsManager::getInstance().getVkInstance().getVkLogicalDevice());
         imguiHelper::Exit();
         ecs.destroy();
         GraphicsManager::getInstance().ShutDown();
-
     }
 
     void Application::Run()
