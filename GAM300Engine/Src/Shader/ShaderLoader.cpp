b2f0f18f420f1c5608f6f6ed8b1773e027b2ef09
EugeneHSS <90674960+EugeneHSS@users.noreply.github.com>
2023-10-31 21:11:57 +0800

    Fixed #1 : Ending crash when exiting application (#82)
    * Big Push
    
    Push Update
    - Revamped Asset Management
    - Model loading and compiling through editor
    - Texture compiler and loading through editor
    - Grid Manager and visual rendering for pathfinding(Incomplete)
    - Fixed vulkan pipeline and texture bugs
    
    Bugs not yet fixed:
    - Imgui crash due to scene frame buffer
    
    * Merge branch 'development' into pr/79
    
    * Fix #1 :  Engine Crash when ending application
    
    ---------
    
    Co-authored-by: Taka <90673673+wending10@users.noreply.github.com>

--- GAM300Engine/Src/Shader/ShaderLoader.cpp
+++ GAM300Engine/Src/Shader/ShaderLoader.cpp
@@ -10,86 +10,88 @@ namespace TDS
             TDS_ERROR("Failed to deserialize shader reflection");
             return;
         }
+
         ShaderMetaData metadata{};
         std::size_t dataSize;
-        file.read(reinterpret_cast<char*>(&dataSize), sizeof(dataSize));
 
-        for (std::size_t i = 0; i < dataSize; ++i)
+        auto readString = [&file]() -> std::string
         {
-            int32_t stageType;
-            file.read(reinterpret_cast<char*>(&stageType), sizeof(stageType));
-            SHADER_FLAG flag = static_cast<SHADER_FLAG>(stageType);
+            std::size_t length;
+            file.read(reinterpret_cast<char*>(&length), sizeof(length));
+            std::string str(length, '\0');
+            file.read(str.data(), length);
+            return str;
+        };
 
-            auto readString = [&file]() -> std::string
-            {
-                std::size_t length;
-                file.read(reinterpret_cast<char*>(&length), sizeof(length));
-                std::string str(length, '\0');
-                file.read(str.data(), length);
-                return str;
-            };
-
-            std::string key = readString();
-            std::string shaderName = readString();
-
-            ShaderData shaderData{};
-            shaderData.m_StageType = flag;
-            shaderData.m_ShaderName = std::move(shaderName);
-
-            file.read(reinterpret_cast<char*>(&shaderData.m_ReflectedData.m_PushConstant), sizeof(PushContantReflect));
-
-            auto readSamplerBufferMap = [&file, &readString](std::map<std::string, SamplerBuffer>& map)
-            {
-                std::size_t mapSize;
-                file.read(reinterpret_cast<char*>(&mapSize), sizeof(mapSize));
-
-                for (std::size_t i = 0; i < mapSize; ++i)
-                {
-                    std::string key = readString();
-                    SamplerBuffer value;
-                    file.read(reinterpret_cast<char*>(&value), sizeof(SamplerBuffer));
-                    map.emplace(std::move(key), std::move(value));
-                }
-            };
-
-            readSamplerBufferMap(shaderData.m_ReflectedData.m_ImageSamplers);
-            readSamplerBufferMap(shaderData.m_ReflectedData.m_StorageImages);
-
-            std::size_t uniformSize{};
-            file.read(reinterpret_cast<char*>(&uniformSize), sizeof(uniformSize));
-
-            for (std::size_t j = 0; j < uniformSize; ++j)
+        auto readSamplerBufferMap = [&file, &readString](std::map<std::string, SamplerBuffer>& map)
+        {
+            std::size_t mapSize;
+            file.read(reinterpret_cast<char*>(&mapSize), sizeof(mapSize));
+            for (std::size_t i = 0; i < mapSize; ++i)
             {
                 std::string key = readString();
-                Uniform uniform;
-                file.read(reinterpret_cast<char*>(&uniform.m_BufferType), sizeof(BUFFER_TYPE));
-                file.read(reinterpret_cast<char*>(&uniform.m_BindingPoint), sizeof(uint32_t));
-                uniform.m_InstanceName = readString();
-                file.read(reinterpret_cast<char*>(&uniform.m_Size), sizeof(size_t));
-
-                std::size_t bufferVariableSize{};
-                file.read(reinterpret_cast<char*>(&bufferVariableSize), sizeof(bufferVariableSize));
-
-                for (std::size_t k = 0; k < bufferVariableSize; ++k)
-                {
-                    BufferVariable bufferVariable;
-                    file.read(reinterpret_cast<char*>(&bufferVariable.m_Size), sizeof(size_t));
-                    file.read(reinterpret_cast<char*>(&bufferVariable.m_Offset), sizeof(size_t));
-                    bufferVariable.m_Name = readString();
-                    file.read(reinterpret_cast<char*>(&bufferVariable.m_Var), sizeof(VAR_TYPE));
-                    uniform.m_UniformsBuffer.push_back(std::move(bufferVariable));
-                }
-
-                shaderData.m_ReflectedData.m_UniformBuffers.emplace(std::move(key), std::move(uniform));
+                SamplerBuffer value;
+                file.read(reinterpret_cast<char*>(&value.m_Location), sizeof(std::uint32_t));
+                file.read(reinterpret_cast<char*>(&value.m_BindingPoint), sizeof(std::uint32_t));
+                file.read(reinterpret_cast<char*>(&value.m_Dimension), sizeof(std::uint32_t));
+                file.read(reinterpret_cast<char*>(&value.m_ArraySize), sizeof(std::uint32_t));
+                value.m_Name = readString();
+                file.read(reinterpret_cast<char*>(&value.m_BufferType), sizeof(BUFFER_TYPE));
+                map.emplace(std::move(key), std::move(value));
             }
+        };
 
-            metadata.m_ShaderDatas.emplace(std::move(key), std::move(shaderData));
+        try
+        {
+            file.read(reinterpret_cast<char*>(&dataSize), sizeof(dataSize));
+            for (std::size_t i = 0; i < dataSize; ++i)
+            {
+                int32_t stageType;
+                file.read(reinterpret_cast<char*>(&stageType), sizeof(stageType));
+                SHADER_FLAG flag = static_cast<SHADER_FLAG>(stageType);
+                std::string key = readString();
+                std::string shaderName = readString();
+                ShaderData shaderData{};
+                shaderData.m_StageType = flag;
+                shaderData.m_ShaderName = std::move(shaderName);
+                file.read(reinterpret_cast<char*>(&shaderData.m_ReflectedData.m_PushConstant), sizeof(PushContantReflect));
+                readSamplerBufferMap(shaderData.m_ReflectedData.m_ImageSamplers);
+                readSamplerBufferMap(shaderData.m_ReflectedData.m_StorageImages);
+                std::size_t uniformSize{};
+                file.read(reinterpret_cast<char*>(&uniformSize), sizeof(uniformSize));
+                for (std::size_t j = 0; j < uniformSize; ++j)
+                {
+                    std::string key = readString();
+                    Uniform uniform;
+                    file.read(reinterpret_cast<char*>(&uniform.m_BufferType), sizeof(BUFFER_TYPE));
+                    file.read(reinterpret_cast<char*>(&uniform.m_BindingPoint), sizeof(uint32_t));
+                    uniform.m_InstanceName = readString();
+                    file.read(reinterpret_cast<char*>(&uniform.m_Size), sizeof(size_t));
+                    std::size_t bufferVariableSize{};
+                    file.read(reinterpret_cast<char*>(&bufferVariableSize), sizeof(bufferVariableSize));
+                    for (std::size_t k = 0; k < bufferVariableSize; ++k)
+                    {
+                        BufferVariable bufferVariable;
+                        file.read(reinterpret_cast<char*>(&bufferVariable.m_Size), sizeof(size_t));
+                        file.read(reinterpret_cast<char*>(&bufferVariable.m_Offset), sizeof(size_t));
+                        bufferVariable.m_Name = readString();
+                        file.read(reinterpret_cast<char*>(&bufferVariable.m_Var), sizeof(VAR_TYPE));
+                        uniform.m_UniformsBuffer.push_back(std::move(bufferVariable));
+                    }
+                    shaderData.m_ReflectedData.m_UniformBuffers.emplace(std::move(key), std::move(uniform));
+                }
+                metadata.m_ShaderDatas.emplace(std::move(key), std::move(shaderData));
+            }
+            m_reflected = std::move(metadata);
         }
-
+        catch (const std::exception& e)
+        {
+            TDS_ERROR("Error deserializing shader reflection: {0}", e.what());
+        }
         file.close();
-        m_reflected = metadata;
-        
     }
+
+
     void ShaderLoader::LoadShader(std::vector<std::uint32_t>& SPIRV, const std::string_view path)
     {
         std::ifstream in(path.data(), std::ios::in | std::ios::binary);
