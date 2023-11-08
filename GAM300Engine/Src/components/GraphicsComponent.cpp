145670f6dadb43e2503856b1a9a08011b95f6a82
thea-sea <90663667+thea-sea@users.noreply.github.com>
2023-11-09 01:13:15 +0800

    Checks for DragDrop, Added EditorHistory (Command History for Undo/Redo) (#104)
    1) for checks on drag drop, filetypes are now checked independently for each PropertyName. for example:
    - if PropertyName.getName() is TextureName, only accept .dds texture files
    - if PropertyName.getName() is ModelName, only accept .bin or .fbx files
    This fixes the issue where initially one could drag a .dds texture into a ModelName box and it changes the input of TextureName box to set texture. After this fix, one must now drag the correct file type to the correct input box.
    2) Implemented a skeleton CommandHistory class, will be linking actions done in imgui windows to the command interface soon. In general, all undo/redo actions will be done on actions on objects in a scene (such translating entity, add entity, remove entity, texturename change). actions such as moving the editor camera, zooming in the editor camera, will not be included for undo/redo.

--- GAM300Engine/Src/components/GraphicsComponent.cpp
+++ GAM300Engine/Src/components/GraphicsComponent.cpp
@@ -36,6 +36,10 @@ namespace TDS
 
 	GraphicsComponent::GraphicsComponent(GraphicsComponent&& toMove) noexcept: m_AssetReference(toMove.m_AssetReference), 
 																			   m_TextureReference(toMove.m_TextureReference), 
-																			   m_ShowMesh(toMove.m_ShowMesh)
+																			   m_ShowMesh(toMove.m_ShowMesh),
+																			   m_TextureName(toMove.m_TextureName),
+																			   m_ModelName(toMove.m_ModelName)
+																			
+																								
 	{ }
 }
