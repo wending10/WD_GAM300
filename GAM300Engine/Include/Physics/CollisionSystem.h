f0dbbcc84175a232491eb834322733802dbce2fd
Taka <90673673+wending10@users.noreply.github.com>
2023-12-05 03:21:32 +0800

    Added vertex & index data for debug drawing (#173)
    * Added player + playercamera
    
    - will be overhauling physics system during the holiday
    
    * bobo ai tracking
    
    * Update Development Game (2) - Copy.json
    
    - Added enemy AI nodes that move around
    
    * semi door interactable
    
    * interaction is done on c++ and CLI side
    
    call IsInteract, instead of isTrigger:
    for interaction:
    - set the door with a sphere collider and set is radius to be 10,
    - set the AImode to Interaction
    - when player is near the door, isInteract will return true
    
    - can call by GetInteract
    
    * bobo collision with wasd done
    
    bobo collision with wasd done
    
    * fix collider
    
    * index & vertex data for sphere/box/capsule
    
    - created index and vertex data for debug drawing
    --> only use for debug drawing as I did not factor in any UV or normals

--- GAM300Engine/Include/Physics/CollisionSystem.h
+++ GAM300Engine/Include/Physics/CollisionSystem.h
@@ -30,7 +30,33 @@ namespace TDS
 		// Main purpose is to get the offset value from each collider and pass to graphics for render
 		static void CollisionSystemUpdate(const float dt, const std::vector<EntityID>& entities, Transform* _transform, GraphicsComponent* _graphics);
 		
+		// Center at 0,0,0, length = 1
+		static std::vector<float> cubeVertices;
+		static std::vector<unsigned int> cubeIndices;
+		// Center at 0,0,0, radius = 1
+		static std::vector<float> sphereVertices;
+		static std::vector<unsigned int> sphereIndices;
+		// Center at 0,0,0, radius = 0.25, height = 1
+		static std::vector<float> capsuleVertices;
+		static std::vector<unsigned int> capsuleIndices;
+
 	private:
+		// Helper functions
+		static void AddVertex(std::vector<float>& vertices, float x, float y, float z);
+		static void AddVertices(std::vector<float>& vertices, const float v1[3], const float v2[3], const float v3[3], const float v4[3]);
+		static void AddIndex(std::vector<unsigned int>& indices, unsigned int a, unsigned int b, unsigned int c);
+		static void ClearAll();
+		// Cube functions
+		static void GenerateCube();
+		
+		// Sphere functions
+		static void GenerateSphere();
+		static std::vector<float> getUnitPositiveX(unsigned int pointsPerRow);
+
+		// Capsule functions
+		static void GenerateCapsule();
+
+
 	};
 }
 
--- GAM300Engine/Src/Physics/CollisionSystem.cpp
+++ GAM300Engine/Src/Physics/CollisionSystem.cpp
@@ -11,10 +11,24 @@
 
 namespace TDS
 {
+	// Static variables
+	std::vector<float>				CollisionSystem::cubeVertices;
+	std::vector<unsigned int>		CollisionSystem::cubeIndices;
+	std::vector<float>				CollisionSystem::sphereVertices;
+	std::vector<unsigned int>		CollisionSystem::sphereIndices;
+	std::vector<float>				CollisionSystem::capsuleVertices;
+	std::vector<unsigned int>		CollisionSystem::capsuleIndices;
+
+
+
 	void CollisionSystem::CollisionSystemInit()
 	{
+		ClearAll();
+		GenerateCube();
+		GenerateSphere();
+		GenerateCapsule();
 	}
-	
+
 	void CollisionSystem::CollisionSystemUpdate(const float dt, const std::vector<EntityID>& entities, Transform* _transform, GraphicsComponent* _graphics)
 	{
 		for (int i = 0; i < entities.size(); ++i)
@@ -32,7 +46,7 @@ namespace TDS
 				Vec3 vScale = Vec3(radius) - Vec3(1.f);
 				_transform[i].SetOffSetScale(vScale);
 			}
-			
+
 			else if (GetBoxCollider(entities[i]))
 			{
 				BoxCollider* vBox = GetBoxCollider(entities[i]);
@@ -52,7 +66,326 @@ namespace TDS
 			}
 		}
 	}
+	/*
+	* Functions that helps to generate shapes vertices and index
+	*/
+	void CollisionSystem::GenerateCube()
+	{
+		cubeVertices =
+		{
+			// Front face
+			-0.5, -0.5,  0.5, // Bottom-left
+			 0.5, -0.5,  0.5, // Bottom-right
+			 0.5,  0.5,  0.5, // Top-right
+			-0.5,  0.5,  0.5, // Top-left
 
-	
+			// Back face
+			-0.5, -0.5, -0.5, // Bottom-left
+			 0.5, -0.5, -0.5, // Bottom-right
+			 0.5,  0.5, -0.5, // Top-right
+			-0.5,  0.5, -0.5  // Top-left
+		};
+
+		cubeIndices =
+		{
+			// Front face
+			0, 1, 2,
+			2, 3, 0,
+
+			// Top face
+			3, 2, 6,
+			6, 7, 3,
+
+			// Back face
+			7, 6, 5,
+			5, 4, 7,
+
+			// Bottom face
+			4, 5, 1,
+			1, 0, 4,
+
+			// Left face
+			4, 0, 3,
+			3, 7, 4,
+
+			// Right face
+			1, 5, 6,
+			6, 2, 1
+
+		};
+	}
+
+	void CollisionSystem::GenerateSphere()
+	{
+		unsigned int vertexCountPerRow = 9; // 2^subdivision + 1, subdivision = 3
+		unsigned int vertexCountPerFace = vertexCountPerRow * vertexCountPerRow;
+
+		std::vector<float> unitVertices = getUnitPositiveX(vertexCountPerRow);
+
+		unsigned int k = 0, k1, k2, i1, i2; // indices
+		float v1[3], v2[3], v3[3], v4[3];   // tmp vertices
+
+
+		// +X face
+		for (unsigned int i = 0; i < vertexCountPerRow - 1; ++i)
+		{
+			k1 = i * vertexCountPerRow;              // index at curr row
+			k2 = k1 + vertexCountPerRow;             // index at next row
+
+			for (unsigned int j = 0; j < vertexCountPerRow - 1; ++j, ++k1, ++k2)
+			{
+				i1 = k1 * 3;
+				i2 = k2 * 3;
+
+				// 4 vertices of a quad
+				// v1--v3
+				// | / |
+				// v2--v4
+				v1[0] = unitVertices[i1];
+				v1[1] = unitVertices[i1 + 1];
+				v1[2] = unitVertices[i1 + 2];
+				v2[0] = unitVertices[i2];
+				v2[1] = unitVertices[i2 + 1];
+				v2[2] = unitVertices[i2 + 2];
+				v3[0] = unitVertices[i1 + 3];
+				v3[1] = unitVertices[i1 + 4];
+				v3[2] = unitVertices[i1 + 5];
+				v4[0] = unitVertices[i2 + 3];
+				v4[1] = unitVertices[i2 + 4];
+				v4[2] = unitVertices[i2 + 5];
+
+				// add 4 vertex attributes
+				AddVertices(sphereVertices, v1, v2, v3, v4);
+
+				// add indices of 2 triangles
+				AddIndex(sphereIndices, k, k + 1, k + 2);
+				AddIndex(sphereIndices, k + 2, k + 1, k + 3);
+
+				k += 4;     // next
+			}
+		}
+
+		// array size and index for building next face
+		unsigned int startIndex;                    // starting index for next face
+		int vertexSize = (int)sphereVertices.size();      // vertex array size of +X face
+		int indexSize = (int)sphereIndices.size();        // index array size of +X face
+
+		// build -X face by negating x and z values
+		startIndex = sphereVertices.size() / 3;
+		for (int i = 0, j = 0; i < vertexSize; i += 3, j += 2)
+		{
+			AddVertex(sphereVertices, -sphereVertices[i], sphereVertices[i + 1], -sphereVertices[i + 2]);
+		}
+		for (int i = 0; i < indexSize; ++i)
+		{
+			sphereIndices.push_back(startIndex + sphereIndices[i]);
+		}
+
+		// build +Y face by swapping x=>y, y=>-z, z=>-x
+		startIndex = sphereVertices.size() / 3;
+		for (int i = 0, j = 0; i < vertexSize; i += 3, j += 2)
+		{
+			AddVertex(sphereVertices, -sphereVertices[i + 2], sphereVertices[i], -sphereVertices[i + 1]);
+		}
+		for (int i = 0; i < indexSize; ++i)
+		{
+			sphereIndices.push_back(startIndex + sphereIndices[i]);
+		}
+
+		// build -Y face by swapping x=>-y, y=>z, z=>-x
+		startIndex = sphereVertices.size() / 3;
+		for (int i = 0, j = 0; i < vertexSize; i += 3, j += 2)
+		{
+			AddVertex(sphereVertices, -sphereVertices[i + 2], -sphereVertices[i], sphereVertices[i + 1]);
+		}
+		for (int i = 0; i < indexSize; ++i)
+		{
+			sphereIndices.push_back(startIndex + sphereIndices[i]);
+		}
+
+		// build +Z face by swapping x=>z, z=>-x
+		startIndex = sphereVertices.size() / 3;
+		for (int i = 0, j = 0; i < vertexSize; i += 3, j += 2)
+		{
+			AddVertex(sphereVertices, -sphereVertices[i + 2], sphereVertices[i + 1], sphereVertices[i]);
+		}
+		for (int i = 0; i < indexSize; ++i)
+		{
+			sphereIndices.push_back(startIndex + sphereIndices[i]);
+		}
+
+		// build -Z face by swapping x=>-z, z=>x
+		startIndex = sphereVertices.size() / 3;
+		for (int i = 0, j = 0; i < vertexSize; i += 3, j += 2)
+		{
+			AddVertex(sphereVertices, sphereVertices[i + 2], sphereVertices[i + 1], -sphereVertices[i]);
+		}
+		for (int i = 0; i < indexSize; ++i)
+		{
+			sphereIndices.push_back(startIndex + sphereIndices[i]);
+		}
+
+
+	}
+
+	std::vector<float> CollisionSystem::getUnitPositiveX(unsigned int pointsPerRow)
+	{
+		std::vector<float> vertices;
+		float n1[3];        // normal of longitudinal plane rotating along Y-axis
+		float n2[3];        // normal of latitudinal plane rotating along Z-axis
+		float v[3];         // direction vector intersecting 2 planes, n1 x n2
+		float a1;           // longitudinal angle along y-axis
+		float a2;           // latitudinal angle
+		float scale;
+
+		// rotate latitudinal plane from 45 to -45 degrees along Z-axis
+		for (unsigned int i = 0; i < pointsPerRow; ++i)
+		{
+			// normal for latitudinal plane
+			a2 = Mathf::Deg2Rad * (45.0f - 90.0f * i / (pointsPerRow - 1));
+			n2[0] = -sin(a2);
+			n2[1] = cos(a2);
+			n2[2] = 0;
+
+			// rotate longitudinal plane from -45 to 45 along Y-axis
+			for (unsigned int j = 0; j < pointsPerRow; ++j)
+			{
+				// normal for longitudinal plane
+				a1 = Mathf::Deg2Rad * (-45.0f + 90.0f * j / (pointsPerRow - 1));
+				n1[0] = -sin(a1);
+				n1[1] = 0;
+				n1[2] = -cos(a1);
+
+				// find direction vector of intersected line, n1 x n2
+				v[0] = n1[1] * n2[2] - n1[2] * n2[1];
+				v[1] = n1[2] * n2[0] - n1[0] * n2[2];
+				v[2] = n1[0] * n2[1] - n1[1] * n2[0];
+
+				// normalize direction vector
+				scale = 1.f / Mathf::Sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+				v[0] *= scale;
+				v[1] *= scale;
+				v[2] *= scale;
+
+				vertices.push_back(v[0]);
+				vertices.push_back(v[1]);
+				vertices.push_back(v[2]);
+
+			}
+		}
+
+		return vertices;
+	}
+
+	void CollisionSystem::GenerateCapsule()
+	{
+		float height = 1.f;
+		float radius = 0.25f;
+		int segments = 20;
+
+		if (segments % 2 != 0)
+		{
+			segments++;
+		}
+
+		int points = segments + 1;
+		float pX[25], pY[25], pZ[25], pR[25];
+
+		float calcH = 0.f;
+		float calcV = 0.f;
+
+		for (int i = 0; i < points; ++i)
+		{
+			pX[i] = Mathf::Sin(calcH * Mathf::Deg2Rad);
+			pZ[i] = Mathf::Cos(calcH * Mathf::Deg2Rad);
+			pY[i] = Mathf::Cos(calcV * Mathf::Deg2Rad);
+			pR[i] = Mathf::Sin(calcV * Mathf::Deg2Rad);
+
+			calcH += 360.f / (float)segments;
+			calcV += 180.f / (float)segments;
+		}
+
+		// Y-offset is half the height minus the diameter
+		float yOff = (height - (radius * 2.f)) * 0.5f;
+		if (yOff < 0)
+			yOff = 0;
+		int top = Mathf::CeilToInt(points * 0.5f);
+		for (int y = 0; y < top; ++y)
+		{
+			for (int x = 0; x < points; ++x)
+			{
+				Vec3 tmp = Vec3(pX[x] * pR[y], pY[y], pZ[x] * pR[y] ) * radius;
+				tmp.y = yOff + tmp.y;
+				AddVertex(capsuleVertices, tmp.x, tmp.y, tmp.z);
+			}
+		}
+
+		// Bottom Hemisphere
+		int btm = Mathf::FloorToInt(points * 0.5f);
+		for (int y = btm; y < points; ++y)
+		{
+			for (int x = 0; x < points; ++x)
+			{
+				Vec3 tmp = Vec3(pX[x] * pR[y], pY[y], pZ[x] * pR[y]) * radius;
+				tmp.y = -yOff + tmp.y;
+				AddVertex(capsuleVertices, tmp.x, tmp.y, tmp.z);
+			}
+		}
+
+		// Index
+		for (int y = 0; y < segments + 1; ++y)
+		{
+			for (int x = 0; x < segments; ++x)
+			{
+				int i0,i1, i2, i3, i4, i5;
+				i0 = (y + 0)* (segments + 1) + x + 0;
+				i1 = (y + 1)* (segments + 1) + x + 0;
+				i2 = (y + 1)* (segments + 1) + x + 1;
+
+				i3 = (y + 0)* (segments + 1) + x + 1;
+				i4 = (y + 0)* (segments + 1) + x + 0;
+				i5 = (y + 1)* (segments + 1) + x + 1;
+
+				AddIndex(capsuleIndices, i0, i1, i2);
+				AddIndex(capsuleIndices, i3, i4, i5);
+			}
+		}
+
+	}
+
+	void CollisionSystem::AddVertex(std::vector<float>& vertices, float x, float y, float z)
+	{
+		vertices.push_back(x);
+		vertices.push_back(y);
+		vertices.push_back(z);
+	}
+
+	void CollisionSystem::AddVertices(std::vector<float>& vertices, const float v1[3], const float v2[3], const float v3[3], const float v4[3])
+	{
+		vertices.insert(vertices.end(), v1, v1 + 3);    // v1
+		vertices.insert(vertices.end(), v2, v2 + 3);    // v2
+		vertices.insert(vertices.end(), v3, v3 + 3);    // v3
+		vertices.insert(vertices.end(), v4, v4 + 3);    // v4
+	}
+
+	void CollisionSystem::AddIndex(std::vector<unsigned int>& indices, unsigned int a, unsigned int b, unsigned int c)
+	{
+		indices.push_back(a);
+		indices.push_back(b);
+		indices.push_back(c);
+	}
+
+	void CollisionSystem::ClearAll()
+	{
+		cubeVertices.clear();
+		cubeIndices.clear();
+		sphereVertices.clear();
+		sphereIndices.clear();
+		capsuleVertices.clear();
+		capsuleIndices.clear();
+	}
+
+
+
 }
 
