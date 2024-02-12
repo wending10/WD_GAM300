7e2791be4f09557df34fedcf09ce649254ce03d1
ruoyango <90823902+ruoyango@users.noreply.github.com>
2024-02-13 03:19:52 +0800

    Added lose screen and changed lockpick assets (#240)
    * Changed pathfinder file to engine
    
    * Added waypoint pathfinding for monster
    
    * merge fix
    
    * update
    
    * Added lockpicking and monster into main game scene
    
    When the lockpick fails, monster will be alerted and will start pathfinding to player
    
    * moving player up
    
    * updated scene
    
    * scene update
    
    * remove of managedScript.sln and using the development M4_Mansion.json
    
    * added lockpicking and door in scene
    
    * Added door functions
    
    * Changed audio and added end credits
    
    * Changed lockpick assets
    
    ---------
    
    Co-authored-by: wending10 <90673673+wending10@users.noreply.github.com>

--- ScriptAPI/Vector2.cxx
+++ ScriptAPI/Vector2.cxx
@@ -11,4 +11,13 @@ namespace ScriptAPI
 	{
 		return lhs.X == X && lhs.Y == Y;
 	}
+	float Vector2::Distance(Vector2 lhs, Vector2 rhs)
+	{
+		return TDS::Vec2::Distance(TDS::floatsToVec2(lhs.X, lhs.Y), TDS::floatsToVec2(rhs.X, rhs.Y));
+	}
+	Vector2 Vector2::MoveTowards(Vector2 lhs, Vector2 rhs, float maxDistanceDelta)
+	{
+		TDS::Vec2 toReturn = TDS::Vec2::MoveTowards(TDS::floatsToVec2(lhs.X, lhs.Y), TDS::floatsToVec2(rhs.X, rhs.Y), maxDistanceDelta);
+		return Vector2(toReturn.x, toReturn.y);
+	}
 }
