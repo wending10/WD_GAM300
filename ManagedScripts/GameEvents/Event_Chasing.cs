6e349de8afe13d56007d3fcaf745813b6d766380
wending10 <90673673+wending10@users.noreply.github.com>
2024-03-11 03:15:36 +0800

    Updated Chasing Scene on Mansion map
    - if got problem hopefully is script problem,  but the required stuff i needed is in the scene

---
+++ ManagedScripts/GameEvents/Event_Chasing.cs
@@ -0,0 +1,69 @@
+﻿using ScriptAPI;
+using System;
+using System.Threading;
+
+public class EventChasing : Script
+{
+
+    public GameObject monster;
+    public GameObject eventChasingTrigger;
+    public GameObject LeftWingLight;
+    public GameObject MainHallLight;
+    public GameObject BedRoomLight;
+    public GameObject BasementLight;
+
+    private float timer = 0.0f;
+    private bool doOnce = true;
+    private bool blinking = false;
+    private bool eventStarted = false;
+    public override void Update()
+    {
+        if (eventStarted == true)
+        {
+            if (doOnce == true)
+            {
+                BedRoomLight.SetActive(false);
+                doOnce = false;
+            }
+
+            // Set a timer for the lights to blink on and off every 1 second with setActive
+            timer += Time.deltaTime;
+            if (timer >= 1.0f)
+            {
+                if (blinking == false)
+                {
+                    LeftWingLight.SetActive(false);
+                    MainHallLight.SetActive(false);
+                    BasementLight.SetActive(false);
+                    blinking = true;
+                }
+                else
+                {
+                    LeftWingLight.SetActive(true);
+                    MainHallLight.SetActive(true);
+                    BasementLight.SetActive(true);
+                    blinking = false;
+                }
+                timer = 0.0f;
+            }
+            if (eventChasingTrigger.GetComponent<RigidBodyComponent>().IsSensorActivated())
+            {
+                LeftWingLight.SetActive(false);
+                MainHallLight.SetActive(false);
+                BasementLight.SetActive(false);
+                eventStarted = false;
+                eventChasingTrigger.GetComponent<ColliderComponent>().SetEnabled(false);
+            }
+        }
+
+
+    }
+
+    public override void OnTriggerEnter(ColliderComponent collider)
+    {
+        monster.GetComponent<GhostMovement>().currentEvent = GhostMovement.GhostEvent.FinalChasingEvent;
+        monster.GetComponent<GhostMovement>().startEvent = true;
+        eventStarted = true;
+        gameObject.GetComponent<ColliderComponent>().SetEnabled(false);
+    }
+}
